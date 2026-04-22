19030ac3eb1f4c4b9291429eb5ed4b48e001012c
Marcial Becerra <marcialbr1820@gmail.com>
2026-04-21 18:01:03 -0600

    Estructura

--- src/socket.js
+++ src/socket.js
@@ -1,12 +1,13 @@
 import { io } from 'socket.io-client';
 
-// Agregamos comillas y https:// para que sea un texto válido
-const URL = import.meta.env.VITE_SOCKET_URL || 'https://serverchatify-production.up.railway.app';
+const URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
 
 export const socket = io(URL, {
     auth: {
         serverOffset: 0,
         ackTimeout: 10000,
         retries: 3,
-    }
+    },
+    
+    autoConnect: true 
 });
